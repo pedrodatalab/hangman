d23fd703048f6b734d700106f860d6dd75e5c698
Pedro Barbosa <datapedrobarbosa@gmail.com>
2025-05-09 16:42:48 -0300

    Alterações na versão 2 do game e inicio da versão 3 focando trabalhar com POO, também gerei um arquivo csv com o chatgpt para aumentar a lista de palavras do jogo

--- Hangman/hangman_v2.py
+++ Hangman/hangman_v2.py
@@ -1,71 +1,166 @@
-# Projeto 1 - Desenvolvimento de Game em Linguagem Python - Versão 1
+# Projeto 1 - Desenvolvimento de Game em Linguagem Python - Versão 2
 
-#import
-
+# Import
 import random
 from os import system, name
 
 # Função para limpar a tela a cada execução
-
 def limpa_tela():
-    # Windows.
+ 
+    # Windows
     if name == 'nt':
         _ = system('cls')
-
-    # Mac ou Linux.
+ 
+    # Mac ou Linux
     else:
-        _ = system ('clear')
+        _ = system('clear')
 
-# Função
+# Função que desenha a forca na tela
+def display_hangman(chances):
 
+    # Lista de estágios da forca
+    stages = [  # estágio 6 (final)
+                """
+                   --------
+                   |      |
+                   |      O
+                   |     \\|/
+                   |      |
+                   |     / \\
+                   -
+                """,
+                # estágio 5
+                """
+                   --------
+                   |      |
+                   |      O
+                   |     \\|/
+                   |      |
+                   |     / 
+                   -
+                """,
+                # estágio 4
+                """
+                   --------
+                   |      |
+                   |      O
+                   |     \\|/
+                   |      |
+                   |      
+                   -
+                """,
+                # estágio 3
+                """
+                   --------
+                   |      |
+                   |      O
+                   |     \\|
+                   |      |
+                   |     
+                   -
+                """,
+                # estágio 2
+                """
+                   --------
+                   |      |
+                   |      O
+                   |      |
+                   |      |
+                   |     
+                   -
+                """,
+                # estágio 1
+                """
+                   --------
+                   |      |
+                   |      O
+                   |    
+                   |      
+                   |     
+                   -
+                """,
+                # estágio 0
+                """
+                   --------
+                   |      |
+                   |      
+                   |    
+                   |      
+                   |     
+                   -
+                """
+    ]
+    return stages[chances]
+
+# Função do jogo
 def game():
 
     limpa_tela()
-
-    print("\nBem-Vindo(a) ao jogo da forca!")
+    print("\nBem vindo ao jogo da forca!")
     print("Adivinhe a palavra abaixo:\n")
-
-    # Lista de palavras para o jogo:
-
+    
+    # Lista de palavras para o jogo
     palavras = ['banana', 'abacate', 'uva', 'morango', 'laranja']
-
-    # Escolhece randomicamente uma palavra
+    
+    # Escolhe randomicamente uma palavra
     palavra = random.choice(palavras)
-
-    #list comprehension
-    letras_descobertas = ['_' for letra in palavra]
-
-    # Chances
+    
+    # Lista  de letras  da palavra
+    lista_letras_palavras = [letra for letra in palavra]
+    
+    # Cria o tabuleiro com o caracter "_" multiplicado pelo comprimento da palavra
+    tabuleiro = ["_"] * len(palavra)
+    
+    # Número de chances
     chances = 6
-
-    # Lista para letras erradas
-    letras_erradas = []
-
+    
+    # Lista para as letras digitadas
+    letras_tentativas = []
+    
+    # Loop enquanto número de chances for maior do que zero
     while chances > 0:
-
-        print(" ".join(letras_descobertas))
-        print("\nChances restantes: ", chances)
-        print("Letras erradas: ", "".join(letras_erradas))
-
-        tentativa = input("\nDigite uma letra: ").lower()
-
-        if tentativa in palavra:
-            index = 0
-            
-            for letra in palavra:
-                if tentativa == letra:
-                    letras_descobertas[index] = letra
-                index += 1
-        else:
-            chances -= 1
-            letras_erradas.append(tentativa)
         
-        if "_" not in letras_descobertas:
-            print("\nVocê venceu, a palavra era:", palavra)
-            break
-    if "_" in letras_descobertas:
-        print("\nVocê perdeu, a palvra era:", palavra)
+        print(display_hangman(chances))
+        print("Palavra: ", tabuleiro)
+        print("\n")
+        
+        # Tentativa
+        tentativa = input("\nDigite uma letra: ")
+        
+        # Condicional
+        if tentativa in letras_tentativas:
+            print("Você já tentou essa letra. Escolha outra!")
+            continue
+        
+        # Lista de tentativas (letras)
+        letras_tentativas.append(tentativa)
+        
+        # Condicional
+        if tentativa in lista_letras_palavras:
+            
+            print("Você acertou a letra!")
+            
+            # Loop
+            for indice in range(len(lista_letras_palavras)):
 
+                # Condicional
+                if lista_letras_palavras[indice] == tentativa:
+                    tabuleiro[indice] = tentativa
+            
+            # Se todos os espaços foram preenchidos, o jogo acabou
+            if "_" not in tabuleiro:
+                print("\nVocê venceu! A palavra era: {}".format(palavra))
+                break
+        else:
+            print("Ops. Essa letra não está na palavra!")
+            # Decremento
+            chances -= 1
+    
+    # Condicional
+    if "_" in tabuleiro:
+        print("\nVocê perdeu! A palavra era: {}.".format(palavra))
+
+
+# Bloco main
 if __name__ == "__main__":
     game()
-    
---
+++ Hangman/hangman_v3.py
@@ -0,0 +1,95 @@
+# Hangman Game (Jogo da Forca) 
+# Programação Orientada a Objetos
+
+# Import
+import random
+
+# Função para limpar a tela a cada execução
+def limpa_tela():
+ 
+    # Windows
+    if name == 'nt':
+        _ = system('cls')
+ 
+    # Mac ou Linux
+    else:
+        _ = system('clear')
+
+# Board (tabuleiro)
+board = ['''
+
+>>>>>>>>>>Hangman<<<<<<<<<<
+
++---+
+|   |
+    |
+    |
+    |
+    |
+=========''', '''
+
++---+
+|   |
+O   |
+    |
+    |
+    |
+=========''', '''
+
++---+
+|   |
+O   |
+|   |
+    |
+    |
+=========''', '''
+
+ +---+
+ |   |
+ O   |
+/|   |
+     |
+     |
+=========''', '''
+
+ +---+
+ |   |
+ O   |
+/|\\  |
+     |
+     |
+=========''', '''
+
+ +---+
+ |   |
+ O   |
+/|\\  |
+/    |
+     |
+=========''', '''
+
+ +---+
+ |   |
+ O   |
+/|\\  |
+/ \\  |
+     |
+=========''']
+
+
+# Classe
+class Hangman:
+     
+	# Método Construtor
+     def __init__(self):
+          pass
+	# Método para adivinhar a letra
+     
+	# Método para verificar se o jogo terminou
+		
+	# Método para verificar se o jogador venceu
+		
+	# Método para não mostrar a letra no board
+		
+	# Método para checar o status do game e imprimir o board na tela
+
