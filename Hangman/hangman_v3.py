76f65a61f3c8f3556507d9bf5cb282beeb79f08b
Pedro Barbosa <datapedrobarbosa@gmail.com>
2025-05-11 00:15:45 -0300

    Terminado método construtor e incluído a função para escolher uma palavra aleatória a partir de um arquivo do excel.

--- Hangman/hangman_v3.py
+++ Hangman/hangman_v3.py
@@ -3,6 +3,9 @@
 
 # Import
 import random
+from os import system, name
+import csv
+
 
 # Função para limpar a tela a cada execução
 def limpa_tela():
@@ -81,8 +84,12 @@ O   |
 class Hangman:
      
 	# Método Construtor
-     def __init__(self):
-          pass
+     def __init__(self, palavra):
+          self.palavra = palavra
+          self.letras_erradas = letras_erradas
+          self.letrascorretas = letras_corretas
+          self.tentativas = tentativas
+          
 	# Método para adivinhar a letra
      
 	# Método para verificar se o jogo terminou
@@ -92,4 +99,17 @@ class Hangman:
 	# Método para não mostrar a letra no board
 		
 	# Método para checar o status do game e imprimir o board na tela
+
+def escolha_palavra():
+    with open(r"E:\Estudos\DSA\Projetos DSA\Hangman\palavras_ptbr.csv", 'r', encoding='utf8') as palavras:
+        linhas = palavras.readlines()
+        palavra = random.choice(linhas).strip()  # .strip() remove quebras de linha
+    
+    print(palavra)
+
+escolha_palavra()
+
+    
+    
+
 
