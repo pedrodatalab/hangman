f30ca951c399a7e096a442a4156fcf9b1311b072
Pedro Barbosa <datapedrobarbosa@gmail.com>
2025-05-13 14:43:48 -0300

    finalização do programa

--- Hangman/hangman_v3.py
+++ Hangman/hangman_v3.py
@@ -84,30 +84,105 @@ O   |
 class Hangman:
      
 	# Método Construtor
-     def __init__(self, palavra):
+    def __init__(self, palavra):
           self.palavra = palavra
-          self.letras_erradas = letras_erradas
-          self.letras_corretas = letras_corretas
-          self.tentativas = tentativas
-          
+          self.letras_erradas = []
+          self.letras_escolhidas = []
+                    
 	# Método para adivinhar a letra
+    def guess(self, letra):
+            
+        if letra in self.palavra and letra not in self.letras_escolhidas:
+            self.letras_escolhidas.append(letra)            
+        elif letra not in self.palavra and letra not in self.letras_erradas:
+            self.letras_erradas.append(letra)            
+        else:
+            return False            
+        return True
      
 	# Método para verificar se o jogo terminou
-		
+    def hangman_over(self):
+
+        return self.hangman_won() or (len(self.letras_erradas) == 6)
+    
 	# Método para verificar se o jogador venceu
-		
+    def hangman_won(self):
+          
+	    if '_' not in self.hide_palavra():
+		    return True
+	    return False
+
 	# Método para não mostrar a letra no board
-		
+    def hide_palavra(self):
+        rtn = ''
+        for letra in self.palavra:
+            if letra not in self.letras_escolhidas:
+                rtn += '_'
+            else:
+                rtn += letra
+        return rtn
+    
 	# Método para checar o status do game e imprimir o board na tela
+    
+    def print_game_status(self):
+           
+        print(board[len(self.letras_erradas)])
+        print ('\nPalavra: ' + self.hide_palavra())
+        print ('\nLetras erradas: ',)
+    
+        for letra in self.letras_erradas:
+            print (letra,)
+            print ()
+            print ('Letras corretas: ',)
+        for letra in self.letras_escolhidas:
+            print (letra,)
+        print ()
+
+# Método para ler uma palavra de forma aleatória do banco de palavras
 
 def escolha_palavra():
     with open(r"E:\Estudos\DSA\Projetos DSA\Hangman\palavras_ptbr.csv", 'r', encoding='utf8') as palavras:
         linhas = palavras.readlines()
         palavra = random.choice(linhas).strip()  # .strip() remove quebras de linha
-    
-    print(palavra)
+        return palavra
 
-escolha_palavra()
+# Método Main - Execução do Programa
+def main():
+
+	limpa_tela()
+
+	# Cria o objeto e seleciona uma palavra randomicamente
+	game = Hangman(escolha_palavra())
+
+	# Enquanto o jogo não tiver terminado, print do status, solicita uma letra e faz a leitura do caracter
+	while not game.hangman_over():
+		
+		#  Status do game
+		game.print_game_status()
+		
+		# Recebe input do terminal
+		user_input = input('\nDigite uma letra: ')
+		
+		# Verifica se a letra digitada faz parte da palavra
+		game.guess(user_input)
+
+	# Verifica o status do jogo
+	game.print_game_status()	
+
+	# De acordo com o status, imprime mensagem na tela para o usuário
+	if game.hangman_won():
+		print ('\nParabéns! Você venceu!!')
+	
+	else:
+		print ('\nGame over! Você perdeu.')
+		print ('A palavra era ' + game.palavra)
+		
+	print ('\nFoi bom jogar com você! Agora vá estudar!\n')
+
+# Executa o programa		
+if __name__ == "__main__":
+	main()
+
 
     
     
